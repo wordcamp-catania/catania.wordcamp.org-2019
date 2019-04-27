2d9a15a6a1b1498b0ef2aeb3b52b644a4bc15df7
Antonino Scarfì <antonino.scarfi@gmail.com>
2019-04-27 16:18:38 +0200

    Include svg sprite for icons

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require("webpack");
+const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -52,5 +54,35 @@ module.exports = {
             append: '\n//# sourceMappingURL=[url]',
             filename: '[file].map'
         }),
+        new SVGSpritemapPlugin('./resources/icons/*.svg', {
+            output: {
+                svg: {
+                    // Disable `width` and `height` attributes on the root SVG element
+                    // as these will skew the sprites when using the <view> via fragment identifiers
+                    sizes: false
+                }
+            },
+            sprite: {
+                generate: {
+                    // Generate <use> tags within the spritemap as the <view> tag will use this
+                    use: true,
+
+                    // Generate <view> tags within the svg to use in css via fragment identifier url
+                    // and add -fragment suffix for the identifier to prevent naming colissions with the symbol identifier
+                    view: '-fragment',
+
+                    // Generate <symbol> tags within the SVG to use in HTML via <use> tag
+                    symbol: true
+                },
+            },
+            styles: {
+                // Specifiy that we want to use URLs with fragment identifiers in a styles file as well
+                format: 'data',
+
+                // Path to the styles file, note that this method uses the `output.publicPath` webpack option
+                // to generate the path/URL to the spritemap itself so you might have to look into that
+                filename: path.join(__dirname, 'src/base/_sprites.less')
+            }
+        })
     ]
 };
