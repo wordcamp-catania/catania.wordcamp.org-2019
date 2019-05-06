82a74b944e9dc83fed51f2c09ca0c1f6414acca5
Antonino Scarfì <antonino.scarfi@gmail.com>
2019-05-06 23:01:52 +0200

    Fix sprite in firefox

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,18 @@ const path = require('path');
 const webpack = require("webpack");
 const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
 module.exports = {
     entry: {
         style: [
             './src/base/2018-orlando.css',
             './src/main.less'
-        ]
+        ],
+        sprite: glob.sync('./resources/icons/*.svg')
     },
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'build')
     },
     module: {
@@ -45,6 +46,13 @@ module.exports = {
                     }
                 }]
             },
+            {
+                test: /\.svg$/,
+                loader: 'svg-sprite-loader',
+                options: {
+                    extract: true
+                }
+            }
         ]
     },
     plugins: [
@@ -56,32 +64,6 @@ module.exports = {
             append: '\n//# sourceMappingURL=[url]',
             filename: '[file].map'
         }),
-        new SVGSpritemapPlugin('./resources/icons/*.svg', {
-            output: {
-                svg: {
-                    // Disable `width` and `height` attributes on the root SVG element
-                    // as these will skew the sprites when using the <view> via fragment identifiers
-                    sizes: false
-                },
-                svg4everybody: true
-            },
-            sprite: {
-                generate: {
-                    // Generate <use> tags within the spritemap as the <view> tag will use this
-                    use: true,
-
-                    // Generate <view> tags within the svg to use in css via fragment identifier url
-                    // and add -fragment suffix for the identifier to prevent naming colissions with the symbol identifier
-                    view: '-fragment',
-
-                    // Generate <symbol> tags within the SVG to use in HTML via <use> tag
-                    symbol: true
-                },
-            },
-            styles: {
-                // Specifiy that we want to use URLs with fragment identifiers in a styles file as well
-                format: 'fragment'
-            }
-        })
+        new SpriteLoaderPlugin()
     ]
 };
